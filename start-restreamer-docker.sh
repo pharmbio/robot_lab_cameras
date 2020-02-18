418ddca8e01ad07eff322dab87e7db79dcf23d6f
andersla <anders@ormbunkar.se>
2020-02-18 21:54:47 +0000

    docker-compose version

--- start-restreamer-docker.sh
+++ start-restreamer-docker.sh
@@ -2,15 +2,20 @@
 docker run -d --restart always \
     --name restreamer \
     -e "RS_USERNAME=admin" \
-    -e "RS_PASSWORD=$RS_PASSWORD" \
+    -e "RS_PASSWORD=passw" \
     -e "RS_MODE=RASPICAM" \
     -e "RS_RASPICAM_WIDTH=1280" \
     -e "RS_RASPICAM_HEIGHT=720" \
+    -e "RS_RASPICAM_FPS=30" \
+    -e "RS_RASPICAM_GOP=30" \
+    -e "RS_RASPICAM_BRIGHTNESS=50" \
+    -e "RS_RASPICAM_BITRATE=5000000" \
     -e "RS_AUDIO=silence" \
     -p 48001:8080 \
     -v /mnt/restreamer/db:/restreamer/db \
     -v /opt/vc:/opt/vc \
     -v "$(pwd)"/restreamer.nginx.conf:/restreamer/conf/nginx.conf \
+    -v "$(pwd)"/player2.html:/restreamer/src/webserver/public/player2.html \
     --privileged \
     datarhei/restreamer-armv7l:latest
 
